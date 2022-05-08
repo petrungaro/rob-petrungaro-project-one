84b165cdfa9a3bb1ee6ecde925bb128d93e21c5b
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-05-08 08:34:53 -0400

    added updated nav and script to all pages

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,14 @@ const slideNav = {};
 slideNav.$menu = $('.menu');
 slideNav.$menuButton =  $('.hamburger');
 slideNav.$window = $(window);
+slideNav.$menuIcon = $('#menu-icon');
 
 
 // INITIALIZATION 
 slideNav.init = function() {
     slideNav.checkScreenWidth();
-    slideNav.$window.resize(slideNav.checkScreenWidth); 
+    slideNav.$window.resize(slideNav.checkScreenWidth);
+    slideNav.rotateIcon(); 
 }
 
 
@@ -17,20 +19,18 @@ slideNav.init = function() {
 
 // Function to check the width of the screen:
 slideNav.checkScreenWidth = function() { 
+    // store the current width of the screen after resizing to a variable:
     const screenSize = slideNav.$window.width();
 
-    console.log(screenSize , 'checking');
-    // if screen width is 500 or under, run mobileMenu
+    // if screen width is 500 or under, run mobileMenu, otherwise show the menu as normal
     if (screenSize <= 500) {
         slideNav.mobileMenu();
-        console.log('under 500, run mobileMenu')
     } else {
         slideNav.$menu.show();
-        console.log('should show menu')
     }
 }
 
-// Function to hide the menu and add a click event listener :
+// Function to hide the menu and add a click event listener (and remove any existing listeners that were applied while resizing the window) :
 slideNav.mobileMenu = function() {
     slideNav.$menu.hide();
     slideNav.$menuButton.off(); 
@@ -41,6 +41,15 @@ slideNav.mobileMenu = function() {
 slideNav.toggleMenu = function() {
     slideNav.$menu.slideToggle("slow");
 }
+
+// Function to toggle class .flip on menu icon :
+slideNav.rotateIcon = function() {
+    slideNav.$menuIcon.on('click', function() {
+        slideNav.$menuIcon .toggleClass('flip');
+    }
+    );
+}
+
 
 //DOCUMENT READY
 $(function() {
