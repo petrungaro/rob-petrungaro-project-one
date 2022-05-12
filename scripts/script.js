c15026c5b69b3dab48d1269d3aea0053a2eec7fd
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-05-12 09:49:16 -0400

    initial commit, added script file and pseudo-code

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// COMMENT SECTION
+
+// add functionality so that comments will be posted to the page when the user submits the form
+
+// Add an event listener to the form, listening for submission
+
+// capture user inputs (name, email, text) and assign them to variables
+// create an html element that will hold the input, matching the comments already on the page ( div.comment-card )
+// add the child elements to the comment-card (img, div.comment-content>p.commenter+p) assign them IDs as well
+
+// add the user inputs to the elements - how? object literals, text nodes, text content, inner text??
+
+
+// append the div as a child to section.comments
+// clear the form
