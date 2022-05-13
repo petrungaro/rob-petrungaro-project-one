86db514f63842e57d40021f618917ae6076f7ae7
Rob Petrungaro <rob.petrungaro@gmail.com>
2022-05-13 13:51:49 -0400

    added html/css/js for a scroll-to-top button that appears when a user has scrolled

---
+++ scripts/scroll.js
@@ -0,0 +1,14 @@
+const scrollButton = document.getElementById('scroll-button');
+
+// when the user scrolls,  show the button
+
+window.addEventListener('scroll', function(){
+    if (window.scrollY > 500) {
+        scrollButton.style.display = 'flex';
+    } 
+}) 
+
+// when the button is clicked, scroll to the top of the page
+scrollButton.addEventListener('click', function() {
+    window.scrollTo(0,0);
+})
